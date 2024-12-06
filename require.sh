c81ce661e3734a0540f67e78be24bf022476f2ee
ews <cxyddsyx@hotmail.com>
2024-12-05 21:06:29 -0600

    requirements_update

---
+++ require.sh
@@ -0,0 +1,4 @@
+pip install torch_geometric
+pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 --index-url https://download.pytorch.org/whl/cpu
+pip install torch-scatter -f https://data.pyg.org/whl/torch-2.2.0+cpu.html
+pip install pandas pytorch_lightning
